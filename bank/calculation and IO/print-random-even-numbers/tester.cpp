baf0ff3b487a1543f02e3bd49a4d170ec248a990
hanizaheri <79909607+hanizaheri@users.noreply.github.com>
2021-03-09 23:11:41 +0330

    Update tester.cpp

--- bank/calculation and IO/print-random-even-numbers/tester.cpp
+++ bank/calculation and IO/print-random-even-numbers/tester.cpp
@@ -1,36 +1,35 @@
-  
 /*
  * tester.cpp
- *
-*/
+ */
 
 #include <iostream>
 #include <fstream>
-#include <string>
 
 using namespace std;
+
+// validate checks existence of r in [0, n] and r parity (r must be even).
+// if r is valid this function returns 0 otherwise 1.
+int validate(int n, int r) {
+    if (r > n || r < 0) {
+        return 1;
+    }
+    return r % 2;
+}
 
 int main(int argc, char const *argv[])
 {
 
-	ifstream test_in(argv[1]);    /* This stream reads from test's input file   */
-	ifstream test_out(argv[2]);   /* This stream reads from test's output file  */
-	ifstream user_out(argv[3]);   /* This stream reads from user's output file  */
+    // opens required files from given files
+    ifstream test_in(argv[1]);    /* This stream reads from test's input file   */
+    // ifstream test_out(argv[2]);   /* This stream reads from test's output file  */
+    ifstream user_out(argv[3]);   /* This stream reads from user's output file  */
 
-	/* Your code here */
-	/* If user's output is correct, return 0, otherwise return 1 */
+    int r1, r2, r3, r4;
+    int n;
 
-	int n, m;
+    test_in >> n;
+    user_out >> r1 >> r2 >> r3 >> r4;
 
-	test_in >> m >> n;
-
-	for (int i = 0; i < 4; i++) {
-		int v;
-
-		user_out >> v;
-		if (v > n || v < m)
-			return 1;
-	}
-
-	return 0;
+    // 0 means user passes the test and 1 otherwise
+    return validate(n, r1) || validate(n, r2) || validate(n, r3) || validate(n, r4);
 }
